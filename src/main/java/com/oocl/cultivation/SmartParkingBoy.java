383bb64d0286f737c2ef7cda809d8275dc245f9d
haohao6683 <852721803@qq.com>
2020-07-27 21:14:44 +0800

    [ParkingLot][Draymond] Refactor: Refactor the park method of SmartParkingBoy.

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -2,7 +2,9 @@ package com.oocl.cultivation;
 
 import exception.ParkingLotException;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 //todo 还要把story6实现
 public class SmartParkingBoy extends ParkingBoy{
@@ -14,22 +16,15 @@ public class SmartParkingBoy extends ParkingBoy{
     @Override
     public Ticket park(Car car) throws ParkingLotException {
         //todo 看能不能不用下面这个比较的对象
-        ParkingLot morePosLot = null;
+        Optional<ParkingLot> validParkingLot = parkingLotList.stream().
+                filter(parkingLot -> !parkingLot.isFull())
+                .max(Comparator.comparingInt(ParkingLot :: getAvaliable));
 
-        for(ParkingLot parkingLot : parkingLotList){
-            if(morePosLot == null){
-                morePosLot = parkingLot;
-            }
-            if(parkingLot.getUsed() < morePosLot.getUsed()){
-                morePosLot = parkingLot;
-            }
+        if(validParkingLot.isPresent()){
+            return validParkingLot.get().park(car);
         }
-        Ticket ticket = morePosLot.park(car);
-
-        if(ticket == null){
+        else{
             throw new ParkingLotException("Not enough position.");
         }
-
-        return ticket;
     }
 }
