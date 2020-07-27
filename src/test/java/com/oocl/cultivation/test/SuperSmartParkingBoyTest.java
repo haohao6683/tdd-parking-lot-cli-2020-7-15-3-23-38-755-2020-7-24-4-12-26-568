478026a6c525954c94ab027a049759b0d8c2d310
haohao6683 <852721803@qq.com>
2020-07-27 21:17:55 +0800

    [ParkingLot][Draymond] Refactor: Remove the useless object and rename the word spuer into super.

--- src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -15,12 +15,11 @@ public class SuperSmartParkingBoyTest extends ParkingBoyTest {
     public void setup() {
         parkingLotA = new ParkingLot(10);
         parkingLotB = new ParkingLot(10);
-        parkingBoy = new ParkingBoy(Arrays.asList(parkingLotA, parkingLotB));
         superSmartParkingBoy = new SuperSmartParkingBoy(Arrays.asList(parkingLotA, parkingLotB));
     }
 
     @Test
-    void should_park_car_which_has_a_larger_available_position_rate_when_spuerSmartParkingBoy_parking() {
+    void should_park_car_which_has_a_larger_available_position_rate_when_superSmartParkingBoy_parking() {
         try {
             //given
             parkingLotA.setUsed(parkingLotA.getCapacity()/2);
