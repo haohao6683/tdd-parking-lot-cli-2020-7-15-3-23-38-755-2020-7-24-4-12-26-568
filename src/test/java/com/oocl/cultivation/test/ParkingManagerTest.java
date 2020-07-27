ad7363166b4d48300ad918549bfed25df6650704
haohao6683 <852721803@qq.com>
2020-07-27 22:36:47 +0800

    [ParkingLot][Draymond] Refactor: Refactor method should_specify_a_parkingBoy_on_the_list_to_park_a_car and remove useless import.

--- src/test/java/com/oocl/cultivation/test/ParkingManagerTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingManagerTest.java
@@ -1,28 +1,33 @@
 package com.oocl.cultivation.test;
 
-import com.oocl.cultivation.Car;
-import com.oocl.cultivation.ParkingBoy;
-import com.oocl.cultivation.ParkingLot;
-import com.oocl.cultivation.Ticket;
+import com.oocl.cultivation.*;
 import exception.ParkingLotException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingManagerTest {
+    protected ParkingLot parkingLot;
+
+    @BeforeEach
+    public void setup() {
+        parkingLot = new ParkingLot(10);
+    }
     @Test
     void should_specify_a_parkingBoy_on_the_list_to_park_a_car() {
         try {
             //given
-            ParkingManager parkingManager = new ParkingManager(new ParkingBoy(Arrays.asList(new ParkingLot(1))));
+            ParkingLot parkingLotA = new ParkingLot(10);
+            ParkingManager parkingManager = new ParkingManager(new ParkingBoy(Arrays.asList(parkingLotA)));
 
             //when
-            Ticket ticket = parkingManager.park(new Car());
+            parkingManager.park(new Car());
 
             //then
-            assertNotNull(ticket);
+            assertEquals(1, parkingLotA.getUsed());
         } catch (ParkingLotException e) {
             e.printStackTrace();
         }
